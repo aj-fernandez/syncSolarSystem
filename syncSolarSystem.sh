70847d97b1634b65ca24ede40616e63bdeeb6c69
aj-fernandez <agustinjfd@outlook.es>
2018-12-05 18:50:09 +0100

    added repo folder

--- syncSolarSystem.sh
+++ syncSolarSystem.sh
@@ -2,12 +2,12 @@
 
 # @author: ajfernandez
 # @last_edit: 2/12/18
-# @Sync myDocs/ folder among my network nodes.
+# @Sync myDocs/ & myRepos/ folders among network nodes.
 
 root="ajfernandez.me"
 nodeName=(
 	"jupiter"
-	"venus"
+#	"venus"
 	"saturn"
 )
 
@@ -16,6 +16,7 @@ totalNodes=${#nodeName[@]}
 for (( i = 0; i < totalNodes; i++ )); do
 	if [ $nodeName[i] != $nodeName[1] ]; then
 		rsync -P -rave ssh /home/ajfernandez/myDocs ajfernandez@"${nodeName[i]}".$root:~/
+		rsync -P -rave ssh /home/ajfernandez/myRepos ajfernandez@"${nodeName[i]}".$root:~/
 	elif [ $nodeName[i] == $nodeName[1] ]; then
 		rsync -P -rav -e "ssh -p 2022" /home/ajfernandez/myDocs ajfernandez@"${nodeName[i]}".$root:~/
 	fi
