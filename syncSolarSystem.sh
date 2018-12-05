958877d7afe71291ae647cbfd8de10865e97fdcc
aj-fernandez <agustinjfd@outlook.es>
2018-12-05 18:01:46 +0100

    extension

--- syncSolarSystem.sh
+++ syncSolarSystem.sh
@@ -17,6 +17,6 @@ for (( i = 0; i < totalNodes; i++ )); do
 	if [ $nodeName[i] != $nodeName[1] ]; then
 		rsync -P -rave ssh /home/ajfernandez/myDocs ajfernandez@"${nodeName[i]}".$root:~/
 	elif [ $nodeName[i] == $nodeName[1] ]; then
-		rsync -P -rav -e "ssh -p2022" home/ajfernandez/myDocs ajfernandez@"${nodeName[i]}".$root:~/
+		rsync -P -rav -e "ssh -p 2022" /home/ajfernandez/myDocs ajfernandez@"${nodeName[i]}".$root:~/
 	fi
 done
