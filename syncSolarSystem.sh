ae2f8a9161953995b5d4189d92b34c41eb901fc4
aj-fernandez <agustinjfd@outlook.es>
2018-12-07 19:55:20 +0100

    minor update

--- syncSolarSystem.sh
+++ syncSolarSystem.sh
@@ -10,12 +10,11 @@ nodeName=(
 	"venus"
 	"saturn"
 )
-i=0
 
 totalNodes=${#nodeName[@]}
 
 for (( i=0 ; i < totalNodes; i++ )); do
-	if [[ "${nodeName[i]}" == "venus" ]]; then
+	if [[ "${nodeName[i]}" == 'venus' ]]; then
 		rsync -P -rav -e "ssh -p 2022" /home/ajfernandez/myDocs /home/ajfernandez/myRepos ajfernandez@"${nodeName[i]}".$root:~/
 	else
 		rsync -P -rave ssh /home/ajfernandez/myDocs /home/ajfernandez/myRepos ajfernandez@"${nodeName[i]}".$root:~/
