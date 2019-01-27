037ca594930cd56dc69f2afa5603f046ac770678
aj-fernandez <agustinjfd@outlook.es>
2019-01-27 23:18:03 +0100

    new folder myLabs

--- syncSolarSystem.sh
+++ syncSolarSystem.sh
@@ -15,8 +15,8 @@ totalNodes=${#nodeName[@]}
 
 for (( i=0 ; i < totalNodes; i++ )); do
 	if [[ "${nodeName[i]}" == 'venus' ]]; then
-		rsync -P -rav -e "ssh -p 2022" /home/ajfernandez/myDocs /home/ajfernandez/myRepos ajfernandez@"${nodeName[i]}".$root:~/
+		rsync -P -rav -e "ssh -p 2022" /home/ajfernandez/myDocs /home/ajfernandez/myRepos /home/ajfernandez/myLabs ajfernandez@"${nodeName[i]}".$root:~/
 	else
-		rsync -P -rave ssh /home/ajfernandez/myDocs /home/ajfernandez/myRepos ajfernandez@"${nodeName[i]}".$root:~/
+		rsync -P -rave ssh /home/ajfernandez/myDocs /home/ajfernandez/myRepos /home/ajfernandez/myLabs ajfernandez@"${nodeName[i]}".$root:~/
 	fi
 done
